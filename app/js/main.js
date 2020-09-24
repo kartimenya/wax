15184042a520417ab9bd7574976ad93fbb1f75f1
Kirill Artimenia <kirill.artimenya@mail.ru>
2020-09-24 21:29:25 +0300

    chage photo

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,15 @@
+const menuBurger = document.getElementsByClassName('menu__burger')[0],
+      menuBbody = document.getElementsByClassName('menu__body')[0]
 
+menuBurger.addEventListener('click', () =>{
+  menuBurger.classList.toggle("active");
+  menuBbody.classList.toggle("active");
+});
 
-$( document ).ready(function() {
-    $('.header__slider').slick({
-      dots: true,
-      dotsClass: 'header__slider-btn',
-      arrows: true,
-      prevArrow: '<button type="button" class="header__slider-prev"><img src="img/back.svg" alt=""></button>',
-      nextArrow: '<button type="button" class="header__slider-next"><img src="img/next.svg" alt=""></button>',
-      responsive: [
-    {
-      breakpoint: 811,
-      settings: {
-        arrows: false,
-      }
-    }
-  ]
-  });
-  $('.posts__slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    infinite: false,
-    prevArrow: '<button type="button" class="posts__slider-prev"><</button>',
-    nextArrow: '<button type="button" class="posts__slider-next">></button>',
-    responsive: [
-  {
-    breakpoint: 910,
-    settings: {
-      slidesToShow: 2,
-    }
-  },
-  {
-    breakpoint: 510,
-    settings: {
-      slidesToShow: 1,
-    }
+document.body.addEventListener('click', (e) =>{
+  let path = e.path;
+  if (!path.includes(menuBurger) && !path.includes(menuBbody)){
+    menuBbody.classList.remove("active");
+    menuBurger.classList.remove("active");
   }
-]
-  });
 });
---
+++ app/js/sliders.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+    $('.header__slider').slick({
+      dots: true,
+      dotsClass: 'header__slider-btn',
+      arrows: true,
+      prevArrow: '<button type="button" class="header__slider-prev"><img src="img/back.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="header__slider-next"><img src="img/next.svg" alt=""></button>',
+      responsive: [
+    {
+      breakpoint: 811,
+      settings: {
+        arrows: false,
+      }
+    }
+  ]
+  });
+  $('.posts__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: false,
+    prevArrow: '<button type="button" class="posts__slider-prev"><</button>',
+    nextArrow: '<button type="button" class="posts__slider-next">></button>',
+    responsive: [
+  {
+    breakpoint: 910,
+    settings: {
+      slidesToShow: 2,
+    }
+  },
+  {
+    breakpoint: 510,
+    settings: {
+      slidesToShow: 1,
+    }
+  }
+]
+  });
+});
